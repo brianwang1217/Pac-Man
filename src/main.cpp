abb26303a07eddc92bfe3793dda7d153c5a3d716
brianwang1217 <brianwang1217@gmail.com>
2018-05-02 09:39:02 -0500

    fixed small sound overoverlap issue, added high score and one-up power-up; actual final touches

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main( ){
     ofSetupOpenGL(1024,768,OF_WINDOW);            // <-------- setup the GL context
     
-    ofSetFrameRate(10);
+    ofSetFrameRate(9);
     // this kicks off the running of my app
     // can be OF_WINDOW or OF_FULLSCREEN
     // pass in width and height too:
